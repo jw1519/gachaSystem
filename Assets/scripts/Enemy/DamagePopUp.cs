b51713013a2aebb184dc91f981e5147302eabafa
Jess <jw1519@student.aru.ac.uk>
2025-09-15 12:55:29 +0100

    damage pop up diassapear after time

--- Assets/scripts/Enemy/DamagePopUp.cs
+++ Assets/scripts/Enemy/DamagePopUp.cs
@@ -3,25 +3,41 @@ using TMPro;
 
 public class DamagePopUp : MonoBehaviour
 {
-    TextMeshPro damageText;
+    TextMeshPro text;
+    Color textColor;
+    float dissapearTimer;
     public static DamagePopUp Create(Vector3 position, int damageAmount)
     {
         Transform damagePopUpTransform = Instantiate(GameAssets.i.damagePopupPrefab, position, Quaternion.identity); 
         DamagePopUp damagePopUp = damagePopUpTransform.GetComponent<DamagePopUp>();
-        damagePopUp.SetDamageText(damageAmount);
+        damagePopUp.SetUp(damageAmount);
         return damagePopUp;
     }
     private void Awake()
     {
-        damageText = GetComponent<TextMeshPro>();
+        text = GetComponent<TextMeshPro>();
     }
     private void Update()
     {
         float moveYSpeed = 2f;
         transform.position += new Vector3(0, moveYSpeed) * Time.deltaTime;
+
+        dissapearTimer -= Time.deltaTime;
+        if (dissapearTimer < 0)
+        {
+            float dissapearSpeed = 3f;
+            textColor.a -= dissapearSpeed * Time.deltaTime;
+            text.color = textColor;
+            if (textColor.a < 0)
+            {
+                Destroy(gameObject);
+            }
+        }
     }
-    public void SetDamageText(int amount)
+    public void SetUp(int amount)
     {
-        damageText.text = amount.ToString();
+        text.text = amount.ToString();
+        textColor = text.color;
+        dissapearTimer = 1f;
     }
 }
